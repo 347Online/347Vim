2bdfbba8cc4f41bf1f6c1d2e837895b3f1b59ba9
347Online | Katie Janzen <katiejanzen@347online.me>
2025-04-28 10:03:52 -0500

    Format repo

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -1,7 +1,7 @@
 vim.g.vimwiki_global_ext = 0
 vim.g.vimwiki_list = { {
-  syntax = "markdown",
-  ext = "md",
-  path = "~/Sync/Notes/wiki/",
+	syntax = "markdown",
+	ext = "md",
+	path = "~/Sync/Notes/wiki/",
 } }
 return { "vimwiki/vimwiki" }
