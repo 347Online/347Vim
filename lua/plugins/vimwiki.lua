89dc8a7f45ff4e0544d12df453c533e008f89164
347Online | Katie Janzen <katiejanzen@347online.me>
2025-05-29 20:00:47 -0500

    Enable vimwiki_auto_header

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -1,7 +1,11 @@
+vim.g.vimwiki_auto_header = 1
 vim.g.vimwiki_global_ext = 0
-vim.g.vimwiki_list = { {
-	syntax = "markdown",
-	ext = "md",
-	path = "~/Sync/Notes/wiki/",
-} }
+vim.g.vimwiki_list = {
+  {
+    syntax = "markdown",
+    ext = "md",
+    path = "~/Sync/Notes/wiki/",
+  }
+}
+
 return { "vimwiki/vimwiki" }
