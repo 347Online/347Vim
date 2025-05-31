1ffd5a84ef3844378ac5e4689188eda51d738b6a
347Online | Katie Janzen <katiejanzen@347online.me>
2025-05-31 16:29:04 -0500

    Stylua

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,26 +1,26 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = { "nvimdev/lspsaga.nvim" },
-  config = function()
-    local lspconfig = require("lspconfig")
-    lspconfig.lua_ls.setup({})
-    lspconfig.ts_ls.setup({})
-    lspconfig.nixd.setup({
-      nixd = {
-        formatting = { command = { "nixfmt" } },
-        nixpkgs = { expr = "import <nixpkgs> { }" },
-        options = {
-          darwin = {
-            expr = '(builtins.getFlake "/Users/katie/347OS").darwinConfigurations.Athena.options',
-          },
-          nixos = {
-            expr = '(builtins.getFlake "/Users/katie/347OS").nixosConfigurations.Aspen.options',
-          },
-          user = {
-            expr = '(builtins.getFlake "/Users/katie/347OS").packages.aarch64-darwin.homeConfigurations.katie.options',
-          },
-        },
-      },
-    })
-  end,
+	"neovim/nvim-lspconfig",
+	dependencies = { "nvimdev/lspsaga.nvim" },
+	config = function()
+		local lspconfig = require("lspconfig")
+		lspconfig.lua_ls.setup({})
+		lspconfig.ts_ls.setup({})
+		lspconfig.nixd.setup({
+			nixd = {
+				formatting = { command = { "nixfmt" } },
+				nixpkgs = { expr = "import <nixpkgs> { }" },
+				options = {
+					darwin = {
+						expr = '(builtins.getFlake "/Users/katie/347OS").darwinConfigurations.Athena.options',
+					},
+					nixos = {
+						expr = '(builtins.getFlake "/Users/katie/347OS").nixosConfigurations.Aspen.options',
+					},
+					user = {
+						expr = '(builtins.getFlake "/Users/katie/347OS").packages.aarch64-darwin.homeConfigurations.katie.options',
+					},
+				},
+			},
+		})
+	end,
 }
--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -1,14 +1,14 @@
 vim.g.vimwiki_auto_header = 1
 vim.g.vimwiki_global_ext = 0
 vim.g.vimwiki_list = {
-  {
-    syntax = "markdown",
-    ext = "md",
-    path = "~/Sync/Notes/",
-  }
+	{
+		syntax = "markdown",
+		ext = "md",
+		path = "~/Sync/Notes/",
+	},
 }
 vim.g.vimwiki_key_mappings = {
-  headers = 0
+	headers = 0,
 }
 
 return { "vimwiki/vimwiki" }
