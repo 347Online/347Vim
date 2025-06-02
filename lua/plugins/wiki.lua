00adcd19e31bb1949e919867053003e0bd550df8
347Online | Katie Janzen <katiejanzen@347online.me>
2025-06-02 13:04:34 -0500

    Switch away from vimwiki
    Now using wiki.vim and nvim-markdown

--- lua/plugins/wiki.lua
+++ lua/plugins/wiki.lua
@@ -3,8 +3,8 @@ vim.g.wiki_global_load = 0
 vim.g.wiki_journal = { name = "diary" }
 
 return {
-  "lervag/wiki.vim",
-  init = function()
-    -- wiki.vim configuration goes here, e.g.
-  end
+	"lervag/wiki.vim",
+	init = function()
+		-- wiki.vim configuration goes here, e.g.
+	end,
 }
