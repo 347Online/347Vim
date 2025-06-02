b1c5b8ec9cde8923644fecdf648a81ff18c1b130
347Online | Katie Janzen <katiejanzen@347online.me>
2025-06-02 13:04:34 -0500

    Switch away from vimwiki
    Now using wiki.vim and nvim-markdown

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,9 @@
+return { "ixru/nvim-markdown" }
+-- return {
+--   "tadmccorkle/markdown.nvim",
+--   -- ft = "markdown", -- or 'event = "VeryLazy"'
+--   event = "VeryLazy",
+--   opts = {
+--     -- configuration here or empty for defaults
+--   },
+-- }
---
+++ lua/plugins/wiki.lua
@@ -0,0 +1,10 @@
+vim.g.wiki_root = "~/Sync/Notes"
+vim.g.wiki_global_load = 0
+vim.g.wiki_journal = { name = "diary" }
+
+return {
+  "lervag/wiki.vim",
+  init = function()
+    -- wiki.vim configuration goes here, e.g.
+  end
+}
