48ef9e599b5d637029eaa39ec0a02b24fd3f2d40
347Online | Katie Janzen <katiejanzen@347online.me>
2025-06-02 20:37:33 -0500

    Basic rust-analyzer config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -22,5 +22,25 @@ return {
 				},
 			},
 		})
+		lspconfig.rust_analyzer.setup({
+			settings = {
+				["rust-analyzer"] = {
+					imports = {
+						granularity = {
+							group = "module",
+						},
+						prefix = "self",
+					},
+					cargo = {
+						buildScripts = {
+							enable = true,
+						},
+					},
+					procMacro = {
+						enable = true,
+					},
+				},
+			},
+		})
 	end,
 }
