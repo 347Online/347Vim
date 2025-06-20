bb3e144681b16ce5ae6e9e03b57a4739201945e8
347Online | Katie Janzen <katiejanzen@347online.me>
2025-06-20 14:22:00 -0500

    Add oil plugins to show git and diagnostic info

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,25 +1,44 @@
 return {
-	"stevearc/oil.nvim",
-	opts = {
-		view_options = {
-			show_hidden = true,
-			is_always_hidden = function(name, bufnr)
-				if
-					string.match(name, "^%.git$")
-					or string.match(name, "^node_modules$")
-					or string.match(name, "^%.%.$")
-					or string.match(name, "^%.stfolder$")
-					or string.match(name, "^%.DS_Store$")
-					or string.match(name, "^%.obsidian$")
-				then
-					return true
-				end
+	{
+		"stevearc/oil.nvim",
+		opts = {
+			view_options = {
+				show_hidden = true,
+				is_always_hidden = function(name, bufnr)
+					if
+						string.match(name, "^%.git$")
+						or string.match(name, "^node_modules$")
+						or string.match(name, "^%.%.$")
+						or string.match(name, "^%.stfolder$")
+						or string.match(name, "^%.DS_Store$")
+						or string.match(name, "^%.obsidian$")
+					then
+						return true
+					end
 
-				return false
-			end,
+					return false
+				end,
+			},
+			win_options = {
+				signcolumn = "yes:2",
+			},
 		},
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+		lazy = false,
 	},
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-	lazy = false,
+	{
+		"refractalize/oil-git-status.nvim",
+
+		dependencies = {
+			"stevearc/oil.nvim",
+		},
+
+		config = true,
+	},
+	{
+		"JezerM/oil-lsp-diagnostics.nvim",
+		dependencies = { "stevearc/oil.nvim" },
+		opts = {},
+	},
 }
