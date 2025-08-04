a927a26d56144bb9a460899c24d4830388ab6121
347Online | Katie Janzen <katiejanzen@347online.me>
2025-08-04 00:34:06 -0500

    Cleanup nvim-markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,9 +1,1 @@
 return { "ixru/nvim-markdown" }
--- return {
---   "tadmccorkle/markdown.nvim",
---   -- ft = "markdown", -- or 'event = "VeryLazy"'
---   event = "VeryLazy",
---   opts = {
---     -- configuration here or empty for defaults
---   },
--- }
