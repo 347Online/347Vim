9e02dbf3ce7b50f0de86d5019f377377c3d9475c
347Online | Katie Janzen <katiejanzen@347online.me>
2025-11-19 00:47:26 -0600

    Migrate away from LSPConfig legacy interface

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,19 +2,18 @@ return {
 	"neovim/nvim-lspconfig",
 	dependencies = { "nvimdev/lspsaga.nvim" },
 	config = function()
-		local lspconfig = require("lspconfig")
-		lspconfig.bashls.setup({})
-		lspconfig.jinja_lsp.setup({})
-		lspconfig.lua_ls.setup({})
-		lspconfig.sqls.setup({})
-		lspconfig.taplo.setup({})
-		lspconfig.ts_ls.setup({})
+		vim.lsp.enable("bashls")
+		vim.lsp.enable("jinja_lsp")
+		vim.lsp.enable("lua_ls")
+		vim.lsp.enable("sqls")
+		vim.lsp.enable("taplo")
+		vim.lsp.enable("ts_ls")
 
-		lspconfig.eslint.setup({
+		vim.lsp.config("eslint", {
 			settings = { useFlatConfig = true },
 		})
 		local flakeExpr = '(builtins.getFlake "' .. vim.fn.expand("$HOME/347OS") .. '")'
-		lspconfig.nixd.setup({
+		vim.lsp.config("nixd", {
 			nixd = {
 				formatting = { command = { "nixfmt" } },
 				nixpkgs = { expr = "import <nixpkgs> { }" },
@@ -32,7 +31,7 @@ return {
 			},
 		})
 
-		lspconfig.rust_analyzer.setup({
+		vim.lsp.config("rust_analyzer", {
 			settings = {
 				["rust-analyzer"] = {
 					imports = {
