b8404be5469695835655783d9bffa83f2d8adc84
347Online | Katie Janzen <katiejanzen@347online.me>
2026-08-14 17:15:29 -0600

    Update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,36 @@
+local ensureInstalled = {
+	"bash",
+	"javascript",
+	"json",
+	"lua",
+	"rust",
+	"tsx",
+	"typescript",
+	"css",
+	"html",
+	"yaml",
+}
+
 return {
 	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = {
-				"bash",
-				"javascript",
-				"json",
-				"lua",
-				"rust",
-				"tsx",
-				"typescript",
-				"css",
-				"html",
-				"yaml",
-			},
-			highlight = { enable = true },
-			indent = { enable = true },
+	branch = "main",
+
+	main = "nvim-treesitter",
+
+	init = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				pcall(vim.treesitter.start)
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
 		})
+
+		local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+		local parsersToInstall = vim.iter(ensureInstalled)
+			:filter(function(parser)
+				return not vim.tbl_contains(alreadyInstalled, parser)
+			end)
+			:totable()
+		require("nvim-treesitter").install(parsersToInstall)
 	end,
 }
